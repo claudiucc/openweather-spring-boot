596093ef87b4c44c432bdae4487f2ff38ee1ba93
Claudiu <peetrr04@gmail.com>
2021-08-03 16:47:23 +0300

    added support for swagger

--- src/main/java/com/assignment/spring/api/util/AppConstants.java
+++ src/main/java/com/assignment/spring/api/util/AppConstants.java
@@ -5,5 +5,5 @@ public class AppConstants {
     public static final String CONTENT_TYPE_HEADER = "Content-Type";
     public static final String ACCEPT_HEADER = "Accept";
     public static final String APP_ID = "8d9c1c7055ec41a33a12e19d3921dfce";
-    public static final String WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}";
+    public static final String WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={appid}";
 }
