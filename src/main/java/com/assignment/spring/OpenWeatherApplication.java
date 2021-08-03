659b588f77f8559121aed89a30e1d988e80f6e9b
Claudiu <peetrr04@gmail.com>
2021-08-03 14:10:23 +0300

    added mapstruct and mapper for weatherDTO

--- src/main/java/com/assignment/spring/OpenWeatherApplication.java
+++ src/main/java/com/assignment/spring/OpenWeatherApplication.java
@@ -2,8 +2,12 @@ package com.assignment.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableSwagger2
 public class OpenWeatherApplication {
 
 	public static void main(String[] args) {
