480d322b8ab6931a3bad48b782e890f47adacfcb
Claudiu <peetrr04@gmail.com>
2021-08-03 14:43:36 +0300

    fixed mapstruct and lombok working together

--- src/main/java/com/assignment/spring/OpenWeatherApplication.java
+++ src/main/java/com/assignment/spring/OpenWeatherApplication.java
@@ -3,11 +3,9 @@ package com.assignment.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableSwagger2
 public class OpenWeatherApplication {
 
 	public static void main(String[] args) {
