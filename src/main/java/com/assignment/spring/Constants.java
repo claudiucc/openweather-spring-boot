fa441694612be55040a409754ad627e90bbb37a0
Claudiu <peetrr04@gmail.com>
2021-08-03 12:35:44 +0300

    added city as pathvariable in weather controller

--- src/main/java/com/assignment/spring/Constants.java
+++ src/main/java/com/assignment/spring/Constants.java
@@ -2,6 +2,6 @@ package com.assignment.spring;
 
 public class Constants {
 
-    public static final String APP_ID = "TBD";
-    public static final String WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={appid}";
+    public static final String APP_ID = "8d9c1c7055ec41a33a12e19d3921dfce";
+    public static final String WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}&appid={appid}";
 }
--- src/main/java/com/assignment/spring/WeatherController.java
+++ src/main/java/com/assignment/spring/WeatherController.java
@@ -4,6 +4,7 @@ import com.assignment.spring.api.WeatherResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -17,9 +18,9 @@ public class WeatherController {
     private RestTemplate restTemplate;
     private WeatherRepository weatherRepository;
 
-    @RequestMapping("/weather")
-    public WeatherEntity weather(HttpServletRequest request) {
-        String city = request.getParameter("city");
+    @RequestMapping("/weather/{city}")
+    public WeatherEntity weather(HttpServletRequest request,
+                                 @PathVariable("city") String city) {
         String url = Constants.WEATHER_API_URL.replace("{city}", city).replace("{appid}", Constants.APP_ID);
         ResponseEntity<WeatherResponse> response = restTemplate.getForEntity(url, WeatherResponse.class);
         return mapper(response.getBody());
