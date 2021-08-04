88b967c73985753239f4ea7a91f233bdd0a0f882
Claudiu <peetrr04@gmail.com>
2021-08-04 10:55:15 +0300

    refactoring the app config

--- src/main/java/com/assignment/spring/OpenWeatherApplication.java
+++ src/main/java/com/assignment/spring/OpenWeatherApplication.java
@@ -1,10 +1,17 @@
 package com.assignment.spring;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
 
 @SpringBootApplication
+@Slf4j
 public class OpenWeatherApplication {
+
+	@Autowired
+	static BuildProperties buildProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenWeatherApplication.class, args);
@@ -12,7 +19,9 @@ public class OpenWeatherApplication {
 	}
 
 	private static void displayVersion() {
-
+		log.info("--------------------------------------------------------------");
+		log.info("Application {} version {} STARTED", buildProperties.getName(), buildProperties.getVersion());
+		log.info("--------------------------------------------------------------");
 	}
 
 }
