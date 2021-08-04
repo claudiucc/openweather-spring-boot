d282623fef34ac8c4b5a3b34c6cc8c158d34105d
Claudiu <peetrr04@gmail.com>
2021-08-04 16:38:34 +0300

    springbootintegrationtests

--- src/main/java/com/assignment/spring/api/model/ErrorDTO.java
+++ src/main/java/com/assignment/spring/api/model/ErrorDTO.java
@@ -1,12 +1,17 @@
 
 package com.assignment.spring.api.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorDTO {
     private String message;
     private LocalDateTime timestamp;
---
+++ src/test/java/com/assignment/spring/api/model/OpenweatherApiTest.java
@@ -0,0 +1,29 @@
+
+package com.assignment.spring.api.model;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class OpenweatherApiTest {
+
+
+    @Test
+    public void testEquals() {
+        OpenweatherApi o1 = new OpenweatherApi();
+        OpenweatherApi o2 = new OpenweatherApi();
+        o1.setServiceSchema("http");
+        o2.setServiceSchema("http");
+        assertTrue(o1.equals(o2));
+        assertTrue(o1.hashCode() == o2.hashCode());
+    }
+}
